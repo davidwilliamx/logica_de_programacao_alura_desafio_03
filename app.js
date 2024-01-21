fab32c08d5d956becf4813a4c8ca32c11dcfdb70
David <david.williamx@gmail.com>
2024-01-21 11:28:09 -0300

    Adiciona resposta desafio 03.

--- app.js
+++ app.js
@@ -22,28 +22,42 @@
  *
  */
 
+// console.log('--- Inicio programa!');
+// let contador = 10;
+// console.log('--- Contador definido.');
+// console.log('--- Inicio do loop.');
+// while (contador >= 0) {
+//     console.log('Iteração número ' + contador);
+//     // Remove - 1 ao valor do contador
+//     contador--;
+// }
+// console.log('--- Fim do loop.');
+// console.log('--- Fim programa!');
+// ---
+
+/**
+ * Crie um programa de contagem regressiva.
+ * Peça um número e conte deste número até 0, usando um loop while no console do navegador.
+ *
+ */
+
 console.log('--- Inicio programa!');
-let contador = 10;
-console.log('--- Contador definido.');
-console.log('--- Inicio do loop.');
-while (contador >= 0) {
-    console.log('Iteração número ' + contador);
-    // Remove - 1 ao valor do contador
-    contador--;
+console.log('--- Entrada do usuário!');
+let limiteContador = Number(prompt('Informe o valor limite do contador: '));
+console.log('--- Limite do contador definido.');
+while (limiteContador >= 0) {
+    console.log('Iteração número ' + limiteContador);
+    // Remove - 1 ao valor do limiteContador
+    limiteContador--;
 }
-console.log('--- Fim do loop.');
+console.log('--- Fim loop!');
 console.log('--- Fim programa!');
 
 // ---
 
 /**
- * Crie um programa de contagem regressiva. Peça um número e conte deste número até 0, usando um loop while no console do navegador.
- *
- */
-// ---
-
-/**
- * Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número, usando um loop while no console do navegador.
+ * Crie um programa de contagem progressiva.
+ * Peça um número e conte de 0 até esse número, usando um loop while no console do navegador.
  *
  */
 // ---
